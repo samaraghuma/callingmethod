0c93ee35c458aabf13158821582d60fee98aba40
raghuma sama <raghuma sama>
2016-10-27 11:53:41 -0400

    first commit

---
+++ ConsoleApplication8/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = sum(4,8);
+            
+            Console.WriteLine(result);
+            
+            
+        }
+
+
+        static int sum(int min,int max)
+        {
+            int sum = 0;
+            for (int i = min; i <= max; i++)
+            {
+                sum = sum + i;
+            }
+            return sum;
+        }
+        static int sum(int max)
+        {
+            int sum = 0;
+            for (int i = 0; i <= max; i++)
+            {
+                sum = sum + i;
+            }
+            return sum;
+        }
+        static int sum()
+        {
+            int sum = 0;
+            for (int i = 0; i<=10; i++)
+            {
+                sum = sum + i;
+            }
+            return sum;
+        }
+    }
+}
